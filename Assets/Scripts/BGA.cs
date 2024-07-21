af368af68bc24772ec44a3e50806abd69584aea6
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-07-22 08:39:06 +0900

    Feat: BGA를 채보 썸네일로 변경

--- Assets/Scripts/BGA.cs
+++ Assets/Scripts/BGA.cs
@@ -1,53 +1,13 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BGA : MonoBehaviour
 {
-    public float speed1 = 0.2f;
-    public float speed2 = 1.2f;
+    public RectTransform canvasRectTransform;
 
-    Image image;
-
-    void Start()
+    public void Init()
     {
-        image = GetComponent<Image>();
-    }
-
-    private void OnEnable()
-    {
-        StartCoroutine(IEParty());
-    }
-
-    IEnumerator IEParty()
-    {
-        float t = 0f;
-        float speed = Random.Range(speed1, speed2);
-
-        Color s = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-        Color d = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-        Color i = Calc(s, d);
-
-        while (true)
-        {
-            if (image != null)
-                image.color = new Color(s.r + i.r * t, s.g + i.g * t, s.b + i.b * t);
-
-            t += Time.deltaTime * speed;
-            if (t > 1f)
-            {
-                t = 0f;
-                speed = Random.Range(speed1, speed2);
-                s = d;
-                d = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-                i = Calc(s, d);
-            }
-            yield return null;
-        }
-    }
-
-    Color Calc(Color start, Color dest)
-    {
-        return new Color((dest.r - start.r), (dest.g - start.g), (dest.b - start.b), 255);
+        UIImage uiBGA = UIController.Instance.FindUI("UI_B_BGA").uiObject as UIImage;
+        uiBGA.SetSprite(GameManager.Instance.sheet.img);
     }
 }
